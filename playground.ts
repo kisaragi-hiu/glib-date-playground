01852982de4e29f06bd1bc0026476e83427cfe18
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-09-07 22:43:44 +0900

    use @pkgjs/parseargs
    Requires a bit of patching: https://github.com/pkgjs/parseargs/issues/157

--- playground.ts
+++ playground.ts
@@ -1,5 +1,19 @@
+import { parseArgs } from "@pkgjs/parseargs";
+// I want this to run on gjs 1.72.2, which complains if you feed imports into it.
+// (The import above is bundled and doesn't reach the runtime.)
 const GLib = imports.gi.GLib;
 
-const date = GLib.DateTime.new_now_local();
+function main() {
+  const parsedArgs = parseArgs({
+    args: ARGV,
+    options: {
+      format: { type: "string" },
+    },
+  });
+  const date = GLib.DateTime.new_now_local();
 
-print(date.format("%FT%T%z"));
+  const format = parsedArgs.values.format ?? "%FT%T%z";
+  print(date.format(format));
+}
+
+main();
