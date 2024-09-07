c7d16553e481550aa933024ba3f42ee62ca2c4bc
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-09-08 00:09:02 +0900

    interactive session

--- formatGLibDate/formatGLibDate.ts
+++ formatGLibDate/formatGLibDate.ts
@@ -1,7 +1,16 @@
 import { parseArgs } from "@pkgjs/parseargs";
+import type { default as GLibModule } from "@girs/glib-2.0";
 // I want this to run on gjs 1.72.2, which complains if you feed imports into it.
 // (The import above is bundled and doesn't reach the runtime.)
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const { printf } = imports.format;
+
+function formatDate(date: GLibModule.DateTime, format: string, raw?: boolean) {
+  return raw
+    ? date.format(format)
+    : `${JSON.stringify(format)}: ${JSON.stringify(date.format(format))}`;
+}
 
 function main() {
   const parsedArgs = parseArgs({
@@ -17,7 +26,7 @@ function main() {
 
 Format the current time according to the given formats.
 
-If none is given, the default format is %FT%T%z.
+If none is given, an interactive session is started.
 
 Options:
   --help / -h: show help (this message)
@@ -29,19 +38,25 @@ GLib DateTime.format docs:
   https://docs.gtk.org/glib/method.DateTime.format.html`);
   } else {
     const raw = parsedArgs.values.raw;
-    const date = GLib.DateTime.new_now_local();
-    if (!raw) {
-      print(`${date.format("%FT%T%z")}`);
-    }
-    for (const format of parsedArgs.positionals.length === 0
-      ? ["%FT%T%z"]
-      : parsedArgs.positionals) {
-      if (raw) {
-        print(date.format(format));
-      } else {
-        print(
-          `${JSON.stringify(format)}: ${JSON.stringify(date.format(format))}`,
-        );
+    if (parsedArgs.positionals.length === 0) {
+      const stdin = new Gio.DataInputStream({
+        base_stream: new Gio.UnixInputStream({ fd: 0 }),
+        close_base_stream: true,
+      });
+      while (true) {
+        const date = GLib.DateTime.new_now_local();
+        print("Enter format below:");
+        const [format] = stdin.read_line_utf8(null);
+        if (typeof format !== "string") break;
+        print(formatDate(date, format, false));
+      }
+    } else {
+      const date = GLib.DateTime.new_now_local();
+      if (!raw) {
+        print(`${date.format("%FT%T%z")}`);
+      }
+      for (const format of parsedArgs.positionals) {
+        print(formatDate(date, format, parsedArgs.values.raw));
       }
     }
   }
